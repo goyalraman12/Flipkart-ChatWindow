4d26aae6a1f64cc02189022de6860ad1d010a586
Raman Goyal <ramangoyal@Ramans-MacBook-Air.local>
2023-04-09 12:12:16 +0530

    added method to find seperator date of two timestamps

--- src/pages/Chatbox/helper.ts
+++ src/pages/Chatbox/helper.ts
@@ -19,3 +19,39 @@ export const getLastMsgDateTime = (ts: DOMHighResTimeStamp = Date.now()) => {
     return momentTime.format("hh:mm A");
   else return momentTime.format("DD/MM/YYYY");
 };
+
+export const getSeperatorDay = (
+  tsLatest?: DOMHighResTimeStamp,
+  tsPrev?: DOMHighResTimeStamp | null
+) => {
+  const dateLatest = tsLatest ? moment(new Date(tsLatest)) : null;
+  const datePrev = tsPrev ? moment(new Date(tsPrev)) : null;
+
+  const diffLatestAndTodayDate = dateLatest?.diff(moment(new Date()), "days");
+
+  const dateSeperateString =
+    diffLatestAndTodayDate &&
+    diffLatestAndTodayDate > 1 &&
+    diffLatestAndTodayDate < 7
+      ? dateLatest?.format("dddd")
+      : dateLatest?.format("DD/MM/YYYY") ===
+        moment(new Date()).format("DD/MM/YYYY")
+      ? "Today"
+      : diffLatestAndTodayDate === 1
+      ? "Yesterday"
+      : dateLatest?.format("DD/MM/YYYY");
+
+  if (!datePrev && dateLatest) return dateSeperateString;
+  else {
+    const diffOfDates =
+      tsPrev && tsPrev ? dateLatest?.diff(datePrev, "days") : null;
+
+    if (diffOfDates) {
+      return dateSeperateString;
+    } else {
+      return datePrev?.format("DD/MM/YYYY") === dateLatest?.format("DD/MM/YYYY")
+        ? diffOfDates
+        : dateSeperateString;
+    }
+  }
+};
